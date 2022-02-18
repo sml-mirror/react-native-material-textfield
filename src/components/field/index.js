f4b3bca46b6f089a33c0c18dff008868ac946361
alex-shul <work.alex.shul@gmail.com>
2022-02-18 09:02:23 +0300

    #18 - fix

--- src/components/field/index.js
+++ src/components/field/index.js
@@ -25,7 +25,7 @@ function startAnimation(animation, options, callback) {
 }
 
 function labelStateFromProps(props, state) {
-  let { placeholder, defaultValue } = props;
+  let { placeholder, defaultValue, disableLabelAnimation } = props;
   let { text, receivedFocus } = state;
 
   return !!(placeholder || text || (!receivedFocus && defaultValue) || disableLabelAnimation);
